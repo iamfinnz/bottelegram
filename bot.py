60e6a925d8ea611d6822d948158260087710c439
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2024-12-29 02:09:09 +0700

    push ketiga

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By  # Import By here
 import chromedriver_autoinstaller
 import time
 import os
